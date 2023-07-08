5336d3b1750a842e6a60b5e24ee851aa16c0de61
João Vitor Stellet <joaovitorstellet@gmail.com>
2023-07-08 18:28:25 -0300

    atualização com alguns tipos de variáveis

--- src/tipos-variaveis/src/App.java
+++ src/tipos-variaveis/src/App.java
@@ -1,5 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        byte numero = 18; /*
+                           * usamos o byte para número pequenos,
+                           * mesmo que a JVM transforme o byte para o int e nos de
+                           * a mesma resposta
+                           */
+
+        short ano; /*
+                    * Já o int usamos para números um pouco mais extensos
+                    * do que o byte ( que usamos para números menores);
+                    * podemos usar o int para número do tamanho de pagamento de funcionário
+                    * ou tambem para o ano que estamos querendo mostrar;
+                    */
+
+        ano = 2023;
+
+        System.out.printf("Feliz %d%n", ano);
+        System.out.printf("João tem %d%n ", numero);
         System.out.println("Hello, World!");
     }
 }
